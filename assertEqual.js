ebb402d252e653ae3f4c3799d96ace9fdbd1941c
Ryan <cordingley.ryan@gmail.com>
2019-10-14 22:30:06 +0000

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😃😃😃😃 " + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🤢🤢🤢🤢 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
