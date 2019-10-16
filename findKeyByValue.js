00626b0bd34ab93aef8cb6d7ed59bca032e4e333
Ryan <cordingley.ryan@gmail.com>
2019-10-16 22:07:16 +0000

    added findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function (object, value) {
+  let arrayOfKeys = Object.keys(object);
+  for (let i = 0; i < arrayOfKeys.length; i++) {
+    if (object[arrayOfKeys[i]] === value) {
+     return arrayOfKeys[i];
+    }
+  }
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
