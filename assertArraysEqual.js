983031f8924a19b6fd482f7b4e7e894a7e7bcf6c
Ryan <cordingley.ryan@gmail.com>
2019-10-21 20:56:24 +0000

    added new file for eqarrays test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,13 @@
-const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(array1, array2) {
-    for (let i = 0; i < array2.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😃😃😃😃 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🤢🤢🤢🤢 Assertion Failed: ${array1} !== ${array2}`);
+  }
 };
 
-module.exports = eqArrays;
+module.exports = assertArraysEqual;
+
--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😃😃😃😃 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
 //loop through array1 and compare values to array 2
   for (let i = 0; i < array2.length; i++) {
@@ -16,8 +8,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //  => should PASS
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+module.exports = eqArrays;
